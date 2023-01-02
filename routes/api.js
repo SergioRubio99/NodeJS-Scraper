a79c676ed6c58e202c331e9314dbbe349896de53
VartonX <legacyentertainmentyt@gmail.com>
2023-01-03 00:23:22 +0100

    I discoveredr the page.$ eval method from the docs, which allows me to take the URL value from the href attribute

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,20 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    
     async function scraper() {
         const headless_browser = await puppeteer.launch({});
         const page = await headless_browser.newPage();
         await page.goto('https://news.ycombinator.com/')
-        let element = await page.waitForSelector(".athing > .title > .titleLine > a")
-        console.log(element);
-        var text = await page.evaluate(element => element.textContent, element)
-        console.log(text)
-        headless_browser.close()
+        let article_title = await page.waitForSelector(".athing > .title > .titleLine > a");
+        console.log(article_title);
+        let titleText = await page.evaluate(article_title => article_title.textContent, article_title);
+
+        let article_url = await page.$eval(".titleline > a[href]", el => el.getAttribute("href"));
+   
+        console.log(titleText);
+        console.log(article_url);
+
+        headless_browser.close();
     }
     scraper()
 
