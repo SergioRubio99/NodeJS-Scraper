2fd92dc2865cd158b0cf8c1cb7a8ef3574b2c2a3
VartonX <legacyentertainmentyt@gmail.com>
2023-01-02 23:56:16 +0100

    playing with puppeteer docs. I can take the whole page

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,20 @@
+const puppeteer = require('puppeteer')
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
     console.log(process.env.PORT);
-    res.status(200).json({
-        myprofile: "Dummy generated data!"
-    })
+    
+    async function scraper() {
+        const headless_browser = await puppeteer.launch({});
+        const page = await headless_browser.newPage();
+        await page.goto('https://news.ycombinator.com/')
+        let element = await page.waitForSelector("#\33 4223998 > td:nth-child(3) > span:nth-child(1) > a:nth-child(1)")
+
+        browser.close()
+    }
+    scraper()
+
 })
 
 module.exports = router;
