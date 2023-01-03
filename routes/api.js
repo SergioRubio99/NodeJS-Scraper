2713ea27b34bc2cc59e8a4804593a5279494d3dd
VartonX <legacyentertainmentyt@gmail.com>
2023-01-04 00:41:41 +0100

    Use of setTimeout API to check pages taken.Redeclaration of variables.

--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,7 @@ router.get("/", (req, res, next) => {
 
 router.get("/:num", (req, res, next) => {
 
+    let articles_arr = [];
     (async () => {
         try {
             console.log(`
@@ -21,37 +22,39 @@ router.get("/:num", (req, res, next) => {
 
             // We create an empty array:
 
-            const articles_arr = []
 
-            let page_count = req.params.num
+            let sum_of_pages = req.params.num
             console.log(req.params)
-            console.log(page_count)
+            console.log(sum_of_pages)
 
             // We lift a puppeteer instance.
 
             const browser = await puppeteer.launch();
-            for (i = 1; i <= page_count; i++) {
-                console.log("page-count => ", page_count)
+            for (i = 1; i <= sum_of_pages; i++) {
                 const page = await browser.newPage();
                 const response = await page.goto(`https://news.ycombinator.com/?p=${i}`);
                 const body = await response.text();
-                console.log(body);
                 // We set up an instance of the puppeteer result, to send it to parse it with the help of JSDOM  
 
                 const { window: { document } } = new jsdom.JSDOM(body);
 
                 // We declare the function we will use later to fill the array
 
+                let newArrObj = { "page": [] }
                 let getTitlesFunction = function (element) {
-                    articles_arr.push({ "article": element.textContent });
+                    newArrObj.page.push({"article": element.textContent})
                 }
-
+                
                 // I transit the DOM as usual, like any browser does, using the DOM API methods, and with the use of the Array.prototype.forEach() method, I execute the function on every HTML tag containing a news title. There, with the use of element.textContent I extract the text to fill the array. Every article's description will be inside an object.
-
+                
                 document.querySelectorAll('tr > td > span[class="titleline"] > a').forEach(getTitlesFunction);
-                console.log(articles_arr)
+                
+                articles_arr.push(newArrObj)
             }
-
+            setTimeout(() => {
+                console.log(articles_arr)
+                console.log(articles_arr.length)
+            },3000)
 
             // We close the browser
             await browser.close();
