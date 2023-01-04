5438eae2f787994fe8880135224fab111e890233
VartonX <legacyentertainmentyt@gmail.com>
2023-01-04 23:11:36 +0100

    EUREKA!!. Now if I ask the crawler to pick me +7 pages, it will pick them all! Before this commit, it left behind randomly some of them randomly, due to excessive speed of execution

--- middlewares/num.js
+++ middlewares/num.js
@@ -7,45 +7,48 @@ const num = (req, res, next) => {
     (async () => {
       
             // We create an empty array:
-
-
             let sum_of_pages = req.params.num
             console.log(req.params)
             console.log(sum_of_pages)
 
-            // We lift a puppeteer instance.
 
-            const browser = await puppeteer.launch();
-            for (i = 1; i <= sum_of_pages; i++) {
-                const page = await browser.newPage();
-                const response = await page.goto(`https://news.ycombinator.com/?p=${i}`);
-                const body = await response.text();
-                // We set up an instance of the puppeteer result, to send it to parse it with the help of JSDOM  
+            try{
+                for (i = 1; i <= sum_of_pages; i++) {
 
-                const { window: { document } } = new jsdom.JSDOM(body);
+                    //EUREKA!! If I put the browser variable declaration here, lifting a puppeteer instance everytime I iterate, a new browser will be set up to crawl every single page, separately! So no page will be left behind in the process of scraping. 
 
-                // We declare the function we will use later to fill the array
+                    //Before this, with the browser constant declared outside the function's scope, I couldn't scrape more than 7 pages without randomly leaving some of them behind.
 
-                let newArrObj = { "page": [] }
-                let getTitlesFunction = function (element) {
-                    newArrObj.page.push({"article": element.textContent})
+                    const browser = await puppeteer.launch();
+                    const page = await browser.newPage();
+                    const response = await page.goto(`https://news.ycombinator.com/?p=${i}`);
+                    const body = await response.text();
+                    // We set up an instance of the puppeteer result, to send it to parse it with the help of JSDOM  
+    
+                    const { window: { document } } = new jsdom.JSDOM(body);
+    
+                    // We declare the function we will use later to fill the array
+    
+                    let newArrObj = { "page": [] }
+                    let getTitlesFunction = function (element) {
+                        newArrObj.page.push({"article": element.textContent})
+                    }
+                    // I transit the DOM as usual, like any browser does, using the DOM API methods, and with the use of the Array.prototype.forEach() method, I execute the function on every HTML tag containing a news title. There, with the use of element.textContent I extract the text to fill the array. Every article's description will be inside an object.
+                    
+                    document.querySelectorAll('tr > td > span[class="titleline"] > a').forEach(getTitlesFunction);
+                    
+                    articles_arr.push(newArrObj)
+                    await browser.close();
                 }
-                
-                // I transit the DOM as usual, like any browser does, using the DOM API methods, and with the use of the Array.prototype.forEach() method, I execute the function on every HTML tag containing a news title. There, with the use of element.textContent I extract the text to fill the array. Every article's description will be inside an object.
-                
-                document.querySelectorAll('tr > td > span[class="titleline"] > a').forEach(getTitlesFunction);
-                
-                articles_arr.push(newArrObj)
+            }catch(e){
+                console.log(e)
             }
             console.log(articles_arr)
             console.log(articles_arr.length)
       
             // We close the browser
-            await browser.close();
       
     })();
-
- 
 }
 
 module.exports = num;
