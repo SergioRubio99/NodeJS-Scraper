e2091bb0743dfacd66adad2c6f4853b62e2bcd94
VartonX <legacyentertainmentyt@gmail.com>
2023-01-06 15:26:46 +0100

    article test set up! It tests that every article taken is a String, of more than 0 characters long

--- tests/num.test.js
+++ tests/num.test.js
@@ -20,38 +20,46 @@ jest.setTimeout(60000)
 //     })
 
 
-test("Inside the object, there is an array with data inside",
-    async () => {
-        const response = await fetch("http://localhost:3000/2")
-        const json = await response.json()
-        // console.log(`JSON ARR CHECK =>>>
-        
-        // `, Array.isArray(json.nycombinatorscraped))
-        expect(Array.isArray(json.nycombinatorscraped)).toBe(true)
-        expect(json.nycombinatorscraped.length).toBeGreaterThan(0)
-    }
-)
-
-
-
-// test("Check whether or not every article entry is an Object with a String inside",
+// test("Inside the object, there is an array with data inside",
 //     async () => {
-//         const arr = await fetch("http://localhost:3000/2")    
-//         for (i = 0; i < arr.length; i++) {
+//         const response = await fetch("http://localhost:3000/2")
+//         const json = await response.json()
+//         // console.log(`JSON ARR CHECK =>>>
+        
+//         // `, Array.isArray(json.nycombinatorscraped))
+//         expect(Array.isArray(json.nycombinatorscraped)).toBe(true)
+//         expect(json.nycombinatorscraped.length).toBeGreaterThan(0)
+//     }
+// )
 
-//             //With the Object.values() method, we conver the page Objects into arrays, and with forEach we unify all the strings into the same array, so we can iterate and check if the content of every article is correct.    
 
-//             Object.values(arr[i]).forEach((e) => {
 
-//                 // We use "<" instead of "<=" because .length starts countin from 1, and the "i" variable needs to be counted from 0, so we can navigate the array index:    
+test("Check whether or not every article entry is an Object with a String inside",
+    async () => {
+        const response = await fetch("http://localhost:3000/2");
+        const json = await response.json();
+        const arr = json.nycombinatorscraped;
+        console.log(`JSON ARR CHECK =>>>
+        `, json)
 
-//                 for (i = 0; i < e.length; i++) {
-//                     // console.log("element!!! =>")    
-//                     // console.log(e[i].article)
-//                     expect(typeof e[i].article == "string").toBe(true)
-//                 }
-//             })
-//         }
-//     })
+        for (i = 0; i < arr.length; i++) {
+
+            //With the Object.values() method, we conver the page Objects into arrays, and with forEach we unify all the strings into the same array, so we can iterate and check if the content of every article is correct.    
+
+            Object.values(arr[i]).forEach((article_arr) => {
+
+                // We use "<" instead of "<=" because .length starts countin from 1, and the "i" variable needs to be counted from 0, so we can navigate the array index:    
+
+                for (i = 0; i < article_arr.length; i++) {
+                    // console.log("element!!! =>")    
+                    // console.log(e[i].article)
+                    expect(typeof article_arr[i].article == "string").toBe(true)
+                    //We want to test that the String is not 0 characters long:
+                    expect(article_arr[i].article.length).toBeGreaterThan(0)
+
+                }
+            })
+        }
+    })
 
 
