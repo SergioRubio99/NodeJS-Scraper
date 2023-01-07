ffb93b488a797faee0d775b5da2fd1c908b5abf2
VartonX <legacyentertainmentyt@gmail.com>
2023-01-07 21:57:26 +0100

    name change of middlewares folder

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const api = require("../middlewares/api");
+const api = require("../controllers/api");
 
 
 router.get("/", api);
