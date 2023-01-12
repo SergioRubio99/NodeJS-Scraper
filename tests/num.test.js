f314e36ce25940bc700ff582e555549c05224e96
SergioRubio99 <legacyentertainmentyt@gmail.com>
2023-01-12 09:05:00 +0100

    pages_arr test implemented

--- tests/num.test.js
+++ tests/num.test.js
@@ -22,7 +22,7 @@ test("The result of the scrape comes in JSON format",
 
 
 test("Inside the object, there is an array with data inside",
-    async () => {
+     () => {
         // const response = await fetch("http://localhost:3000/2")
         // const json = await response.json()
         // console.log(`JSON ARR CHECK =>>>
@@ -35,30 +35,21 @@ test("Inside the object, there is an array with data inside",
 
 
 
-test("Check whether or not every article entry is an Object with a String inside",
-    async () => {
+test("Check whether or not every page entry is an Object with an Array filled with data",
+     () => {
         // const response = await fetch("http://localhost:3000/2");
         // const json = await response.json();
         const arr = json.nycombinatorscraped;
-        console.log(`JSON ARR CHECK =>>>
-        `, json)
+        console.log(`JSON PAGES ARR CHECK =>>>
+        `, json.nycombinatorscraped)
 
         for (i = 0; i < arr.length; i++) {
+            //With the Object.values() method, we convert the page Objects into arrays, and with forEach we unify all the strings into the same array, so we can iterate and check if the content of every article is correct.    
+            Object.values(arr[i]).forEach((page_arr) => {
+                console.log("PAGE ARR =>", page_arr.length)
+                    expect(Array.isArray(page_arr)).toBe(true)
+                    expect((page_arr.length)).toBeGreaterThan(0)
 
-            //With the Object.values() method, we conver the page Objects into arrays, and with forEach we unify all the strings into the same array, so we can iterate and check if the content of every article is correct.    
-
-            Object.values(arr[i]).forEach((article_arr) => {
-
-                // We use "<" instead of "<=" because .length starts countin from 1, and the "i" variable needs to be counted from 0, so we can navigate the array index:    
-
-                for (i = 0; i < article_arr.length; i++) {
-                    // console.log("element!!! =>")    
-                    // console.log(e[i].article)
-                    expect(typeof article_arr[i].article == "string").toBe(true)
-                    //We want to test that the String is not 0 characters long:
-                    expect(article_arr[i].article.length).toBeGreaterThan(0)
-
-                }
             })
         }
     })
@@ -66,3 +57,17 @@ test("Check whether or not every article entry is an Object with a String inside
 
 
 
+
+    // Object.values(arr[i]).forEach((page_arr) => {
+
+    //     // We use "<" instead of "<=" because .length starts countin from 1, and the "i" variable needs to be counted from 0, so we can navigate the array index:    
+
+    //     for (i = 0; i < page_arr.length; i++) {
+    //         // console.log("element!!! =>")    
+    //         // console.log(e[i].article)
+    //         expect(typeof page_arr[i].article == "string").toBe(true)
+    //         //We want to test that the String is not 0 characters long:
+    //         expect(page_arr[i].article.length).toBeGreaterThan(0)
+
+    //     }
+    // })
