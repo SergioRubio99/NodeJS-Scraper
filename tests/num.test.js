31abaa208b8ea946c94d2b817aa4b4b1aa9cc8e1
SergioRubio99 <legacyentertainmentyt@gmail.com>
2023-01-12 09:49:09 +0100

    article fields test implemented

--- tests/num.test.js
+++ tests/num.test.js
@@ -40,8 +40,8 @@ test("Check whether or not every page entry is an Object with an Array filled wi
         // const response = await fetch("http://localhost:3000/2");
         // const json = await response.json();
         const arr = json.nycombinatorscraped;
-        console.log(`JSON PAGES ARR CHECK =>>>
-        `, json.nycombinatorscraped)
+        // console.log(`JSON PAGES ARR CHECK =>>>
+        // `, json.nycombinatorscraped)
 
         for (i = 0; i < arr.length; i++) {
             //With the Object.values() method, we convert the page Objects into arrays, and with forEach we unify all the strings into the same array, so we can iterate and check if the content of every article is correct.    
@@ -56,6 +56,74 @@ test("Check whether or not every page entry is an Object with an Array filled wi
 
 
 
+
+    test("Check whether or not every article is an Object with 5 entries",
+    () => {
+       // const response = await fetch("http://localhost:3000/2");
+       // const json = await response.json();
+       const arr = json.nycombinatorscraped;
+        //    console.log(`JSON PAGES ARR CHECK =>>>
+        //    `, json.nycombinatorscraped);
+
+       for (i = 0; i < arr.length; i++) {
+           //With the Object.values() method, we convert the page Objects into arrays, and with forEach we unify all the strings into the same array, so we can iterate and check if the content of every article is correct.    
+           Object.values(arr[i]).forEach((page_arr) => {
+            //    console.log("PAGE ARR =>", page_arr)
+                page_arr.forEach(article => expect(Object.entries(article).length).toBe(6))
+           })
+       }
+   })
+
+   test("Check fields: title, url, points, user, creationDate, comments",
+   () => {
+      // const response = await fetch("http://localhost:3000/2");
+      // const json = await response.json();
+      const arr = json.nycombinatorscraped;
+      console.log(`JSON PAGES ARR CHECK =>>>
+      `, json.nycombinatorscraped);
+
+      for (i = 0; i < arr.length; i++) {
+        //With the Object.values() method, we convert the page Objects into arrays, and with forEach we unify all the strings into the same array, so we can iterate and check if the content of every article is correct.
+        Object.values(arr[i]).forEach((page_arr) => {
+          //Inside the pages arr, we iterate over every article, and inside every article we iterate over every element:
+          console.clear();
+          page_arr.forEach((article) => {
+            let articleObjArr = Object.values(article);
+            //-----------------------------
+            //title test:
+            expect(typeof articleObjArr[0]).toBe("string");
+            expect(articleObjArr[0].length).toBeGreaterThan(0);
+            // console.log(articleObjArr[0]);
+            //-----------------------------
+            // url test:
+            expect(typeof articleObjArr[1]).toBe("string");
+            expect(articleObjArr[1].length).toBeGreaterThan(8);
+            // console.log(articleObjArr[1]);
+            //-----------------------------
+            // points test:
+            expect(typeof articleObjArr[2]).toBe("number");
+            expect(articleObjArr[2] < 0).toBe(false);
+            // console.log(articleObjArr[2]);
+            //-----------------------------
+            // user test:
+            expect(typeof articleObjArr[3]).toBe("string");
+            expect(articleObjArr[3].length).toBeGreaterThan(1);
+            // console.log(articleObjArr[3]);
+            //-----------------------------
+            // creationDate test:
+            expect(typeof articleObjArr[4]).toBe("string");
+            //Why do I choose 7 as minimum length required? Because it is the length of the termm "unknown". This is what getAge() function will deliver if no date is placed into the array
+            expect(articleObjArr[4].length).toBeGreaterThan(6);
+            //-----------------------------
+            // comments test:
+            expect(articleObjArr[5].length).toBeGreaterThan(3);
+            console.log(articleObjArr[5]);
+
+            return;
+          });
+        });
+      }
+  })
 
 
     // Object.values(arr[i]).forEach((page_arr) => {
