cb7790376e43d373f0f0ff5cd01b2d75bb52cdab
SergioRubio99 <legacyentertainmentyt@gmail.com>
2023-01-15 02:44:42 +0100

    get age function implementation (buildLowerArticle.js > article_age.js). Now we scrape the age of each article successfully

--- functions/article_functions/article_age.js
+++ functions/article_functions/article_age.js
@@ -1,5 +1,13 @@
-let getAge = () => {
-
+let getAge = (element) => {
+    if (!element.querySelectorAll(".subline> .age")[0]) {
+        // In some articles, age is located under the a different selector:
+        if(element.querySelectorAll(".subtext > .age > a")){
+          let age = element.querySelectorAll(".subtext > .age > a")[0].textContent;
+          return age.slice(0,-4); //
+        }
+      }
+      let age = element.querySelectorAll(".subline> .age")[0].textContent;
+      return age.slice(0,-4);
 }
 
 module.exports = getAge;
