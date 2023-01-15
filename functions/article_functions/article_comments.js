f38bcc75000b564b99e97fe8baccad7df88c6a8e
SergioRubio99 <legacyentertainmentyt@gmail.com>
2023-01-15 19:02:27 +0100

    Implementation of the getDOM() function. It gives the page's DOM to the crawler

--- functions/article_functions/article_comments.js
+++ functions/article_functions/article_comments.js
@@ -1,11 +1,13 @@
 let getComments = (element) => {
-  let comments = element.querySelectorAll(".subline > a:nth-child(6)")[0].textContent;
-  // Here, we're looking for the possible number of comments of the article. We extract the number, and in case no number is found, we send 0 as the number of total comments. We try to match with 4,3,2 and 1 digit numbers: 
-
+    // Here, we're looking for the possible number of comments of the article. We extract the number, and in case no number is found, we send 0 as the number of total comments. We try to match with 4,3,2 and 1 digit numbers: 
+  
   if (!element.querySelectorAll(".subline > a:nth-child(6)")[0]) {
     commentsNum = 0;
     return commentsNum;
   }
+  
+  let comments = element.querySelectorAll(".subline > a:nth-child(6)")[0].textContent;
+
   if (/[0-9]{4}/.test(comments)) {
     commentsNum = parseInt(comments.match(/[0-9]{4}/)[0]);
     return commentsNum;
--- functions/crawler.js
+++ functions/crawler.js
@@ -1,18 +1,10 @@
-const axios = require("axios");
-const jsdom = require("jsdom");
 const buildUpperArticle = require("./article_functions/buildUpperArticle");
 const buildLowerArticle = require("./article_functions/buildLowerArticle");
-
-const axiosInstance = axios.create({
-  baseURL: "https://news.ycombinator.com/",
-  timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" },
-});
+const DOM = require("./getDOM");
 
 const crawler = async (pages) => {
   pages === undefined ? (pages = 1) : undefined;
-  let page = await axiosInstance.get();
-  const { document } = new jsdom.JSDOM(page.data).window;
+  const { document } = await DOM(pages);
   let upperArr = [];
   let lowerArr = [];
   //First, we call buildUpperArticle() to scrape the first half of the article:
