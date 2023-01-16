291125971827ef2ecfc7873cab54882288947001
SergioRubio99 <legacyentertainmentyt@gmail.com>
2023-01-16 21:33:54 +0100

    Add error handling and improve comments

--- controllers/api.js
+++ controllers/api.js
@@ -3,26 +3,31 @@ const saveCache = require("../functions/cache").saveCache;
 const getCache = require("../functions/cache").getCache;
 
 module.exports = async (req, res) => {
-  //We take the num of pages requested by client in the URL, and check if its existing. In affirmative case, nothing done, in the opposite case, that value is 1.
+  try {
+    //validation to ensure that a number is entered
+    let pages_to_scrape = isNaN(parseInt(req.params.num)) ? 1 : parseInt(req.params.num);
+    articles_arr = [],
+    isCacheEmpty = Object.keys(getCache().data).length === 0;
 
-  let pages_to_scrape = isNaN(parseInt(req.params.num)) ? 1 : parseInt(req.params.num);
-  articles_arr = [],
-  isCacheEmpty = Object.keys(getCache().data).length === 0;
+    // Check if cache is empty, if so, initialize it with an empty array
+    let cachePages = !isCacheEmpty ? getCache().data["pages"]["v"] : [];
+    // Set the starting page for crawling using the cache length
+    crawlFromPage = Object.keys(cachePages).length / 30+1;
 
-  //If the cache is empty, the cache will be filled with an Array. If it already has data, a new variable (cachePages) will contain that data. Then we define the page the loop will start crawling from (crawlFromPage), using the cache length.
+    // Add cache pages to articles array up to the number of pages requested
+    articles_arr.push(...cachePages.slice(0, pages_to_scrape * 30));
+    console.log(cachePages.length)
 
-  let cachePages = !isCacheEmpty ? getCache().data["pages"]["v"] : [];
-  crawlFromPage = Object.keys(cachePages).length / 30+1;
-
-  //The slice method default behavior allows to input a second value (until where to substract) larger than the arrays length. In this case, it will cut until there. This is useful in those cases when the number of articles requested by client is lesser than the total of articles cached.
-  articles_arr.push(...cachePages.slice(0, pages_to_scrape * 30));
-  console.log(cachePages.length)
-  
-  //We start crawling:
-  for (i = crawlFromPage; i <= pages_to_scrape; i++) {
-    let page = await crawler(i);
-    articles_arr.push(page);
+    //Start crawling:
+    for (i = crawlFromPage; i <= pages_to_scrape; i++) {
+      let page = await crawler(i);
+      console.log(`Page ${i} crawled!`);
+      articles_arr.push(page);
+    }
+    if(crawlFromPage < pages_to_scrape) saveCache(articles_arr.flat());
+    return res.status(200).json({ nycombinator: articles_arr.flat() });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error al procesar la solicitud" });
   }
-  if(crawlFromPage < pages_to_scrape) saveCache(articles_arr.flat());
-  return res.status(200).json({ nycombinator: articles_arr.flat() });
 };
