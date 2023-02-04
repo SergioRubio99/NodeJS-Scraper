7038fa7ce7f479a2c48a65fc35ed79207caa072e
SergioRubio99 <legacyentertainmentyt@gmail.com>
2023-02-04 15:50:52 +0100

    Put router in index file, instead of putting it on a separate file

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
-const api = require("./routes/router");
+const api = require("./controllers/main_controller");
 require('dotenv').config();
 
-app.use("/", api);
-console.clear();
+app.get("/", api);
+app.get("/:num", api);
 
-
-app.listen(3000);
-console.log("Listening, on the 3000 port");
+app.listen(3000), console.log("Listening, on the 3000 port");
 
 module.exports = app;
+ 
--- routes/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const api = require("../controllers/main_controller");
-
-router.get("/", api);
-router.get("/:num", api);
-
-module.exports = router;
