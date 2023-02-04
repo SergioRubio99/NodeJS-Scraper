7c3a44cfedaafdfe404a4425f7a6f6ba17faf80d
SergioRubio99 <legacyentertainmentyt@gmail.com>
2023-02-04 15:53:52 +0100

    Merge the only two API endpoints into a unique one

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const api = require("./controllers/main_controller");
 require('dotenv').config();
 
-app.get("/", api);
-app.get("/:num", api);
+app.get("/:num?", api);
 
 app.listen(3000), console.log("Listening, on the 3000 port");
 
